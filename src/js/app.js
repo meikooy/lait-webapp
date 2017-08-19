c519a57d4c41aa880f216fd24554959d5c6b1c02
mikkokut <mikko93@gmail.com>
2017-08-19 18:04:19 +0300

    track searches

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,8 @@ import {index} from './services/algolia';
 import SearchInput from './components/search/search-input';
 import SearchResults from './components/search/search-results';
 
+import debounce from 'debounce';
+
 export default class App extends Component {
   constructor() {
     super();
@@ -22,6 +24,15 @@ export default class App extends Component {
     let searchWord = e.target.value;
 
     if (searchWord && searchWord.length) {
+
+      // Track analytics
+      if (window.ga) {
+        const track = function() {
+            ga('send', 'pageview', '/?q=' + searchWord);
+        };
+        debounce(track, 500)();
+      }
+
       this.setState({loading: true});
 
       index.search(e.target.value, (err, content) => {
